088f1e6a524641dc54e0e758b0b6eb85c5faa9fd
Yuivika05 <yuvikamishra05@gmail.com>
2025-01-10 23:58:28 +0530

    Update UserServlet.java

--- src/main/java/com/translator/servlet/UserServlet.java
+++ src/main/java/com/translator/servlet/UserServlet.java
@@ -1,1 +1,45 @@
+package com.userapp.servlet;
 
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet("/user/*")
+public class UserServlet extends HttpServlet {
+    private UserDAO userDAO;
+    
+    @Override
+    public void init() {
+        userDAO = new UserDAO();
+    }
+    
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+            throws ServletException, IOException {
+        String action = request.getPathInfo();
+        switch (action) {
+            case "/register":
+                showRegistrationForm(request, response);
+                break;
+            case "/profile":
+                showProfile(request, response);
+                break;
+            case "/list":
+                listUsers(request, response);
+                break;
+        }
+    }
+    
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) 
+            throws ServletException, IOException {
+        String action = request.getPathInfo();
+        switch (action) {
+            case "/register":
+                registerUser(request, response);
+                break;
+            case "/update":
+                updateProfile(request, response);
+                break;
+        }
+    }
+}
